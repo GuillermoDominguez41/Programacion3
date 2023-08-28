0074dfde5318419b44c7e88b7eea9c09a72df09a
GuillermoDominguez41 <83877647+GuillermoDominguez41@users.noreply.github.com>
2023-08-28 17:54:04 -0300

    Update Main.java
    -Generación del constructor
    -Generación de los métodos createRandomBoard,  showBoard y getRandomBoard.

--- src/lightsOut/Main.java
+++ src/lightsOut/Main.java
@@ -1,7 +1,46 @@
 package lightsOut;
 
+import java.util.Random;
+
 public class Main {
 
+	private Integer [][] randomBoard;
+	private Integer sizeBoard;
+
+	public Main(Integer size) {
+		sizeBoard = size;
+		randomBoard = createRandomBoard();
+	}
 	
+	protected Integer[][] createRandomBoard() {
+		Integer [][] board = new Integer[sizeBoard][sizeBoard];
+		Random rand = new Random();
+		for (Integer row = 0; row < board.length; row++) {
+			for (Integer col = 0; col < board[row].length; col++) {
+				board[row][col] = rand.nextInt(2);
+			}
+		}
+		 // ONLY FOR TESTING FUNCTIONS !!!
+			showBoard(board);
+		 // END OF TEST
+		return board;
+	}
+	
+	public Integer [][] getRandomBoard() {
+		return randomBoard;
+	}
+	
+	 // ONLY FOR TESTING FUNCTIONS !!!
+		protected void showBoard(Integer [][] board) {	
+			StringBuilder st = new StringBuilder();
+			for (Integer row = 0; row < board[0].length; row++) {
+				for (Integer col = 0; col < board[0].length; col++) {
+					st.append(board[row][col] + " ");
+				}
+				st.append("\n");
+			}
+			System.out.print( st.toString()+"\n");	
+		}
+	 // END OF TEST
 	
 }
