2a0b5d25fc4dabeeff745540031724c6aa6c1192
GuillermoDominguez41 <83877647+GuillermoDominguez41@users.noreply.github.com>
2023-08-28 17:53:51 -0300

    Update Controller.java
    -Generación del constructor
    -Incorporación del objeto Main en la clase Controller.
    -Generación de método getRandomBoard.

--- src/lightsOut/Controller.java
+++ src/lightsOut/Controller.java
@@ -1,5 +1,14 @@
 package lightsOut;
 
 public class Controller {
+	Main MN;
 
+	public Controller(Integer size) {
+		MN = new Main(size);
+	}
+	
+	public Integer[][] getRandomBoard() {
+		return MN.getRandomBoard();
+	}
+	
 }
