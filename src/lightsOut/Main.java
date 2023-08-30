ffff1da03e044ddd72d519369bc8cad7c15c8917
GuillermoDominguez41 <83877647+GuillermoDominguez41@users.noreply.github.com>
2023-08-30 17:11:04 -0300

    Update Main.java
    updateBoard: Al modificar la referencia de posición integrando el índice, tuve que modificar el método de obtención de fila y columna.
    toggleValueRow: Refatorice el metodo para alternar valores de un IF a un IF Ternario.
    toggleValueColumn: Idem toggleValueRow

--- src/lightsOut/Main.java
+++ src/lightsOut/Main.java
@@ -20,7 +20,7 @@ public class Main {
 				board[row][col] = rand.nextInt(2);
 			}
 		}
-		// ONLY FOR TESTING FUNCTIONS !!!
+		// ONLY FOR TESTING!!!
 		showBoard();
 		// END OF TEST
 		return board;
@@ -36,43 +36,26 @@ public class Main {
 	
 	public void updateBoard(String position) {
 		Integer row, col;
-
 		if (position != null) {
-			row = Integer.parseInt(position.charAt(0) + "");
-			col = Integer.parseInt(position.charAt(2) + "");
-
+			row = Integer.parseInt( position.substring(4, 6).replace(" ", "") );
+			col = Integer.parseInt( position.substring(10, 12).replace(" ", "") );
 			toggleValueRow(row);
 			toggleValueColumn(row, col);
-
 		}
-
 	}
 
 	public void toggleValueRow(Integer row) {
-
 		for (Integer col = 0; col < board[0].length; col++) {
-
 			Integer currentItem = board[row][col];
-			if (currentItem == 0) {
-				board[row][col] = 1;
-			} else {
-				board[row][col] = 0;
-			}
+			board[row][col] = currentItem == 0 ? 1 : 0;
 		}
 	}
 
 	public void toggleValueColumn(Integer row, Integer col) {
-
 		for (Integer fil = 0; fil < board[0].length; fil++) {
 			Integer currentItem = board[fil][col];
-
 			if (fil != row) {
-
-				if (currentItem == 0) {
-					board[fil][col] = 1;
-				} else {
-					board[fil][col] = 0;
-				}
+				board[fil][col] = currentItem == 0 ? 1 : 0;
 			}
 		}
 	}
